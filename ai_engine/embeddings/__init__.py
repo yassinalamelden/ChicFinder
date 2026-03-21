9935ce320da9f5945ec7eebae68a26251e841206
yassin.alamelden <yassin.alamelden@gmail.com>
2026-03-21 18:43:49 +0200

    feat: introduce `ai_engine.embeddings` package by adding its public API and updating dependencies.

--- ai_engine/embeddings/__init__.py
+++ ai_engine/embeddings/__init__.py
@@ -1,1 +1,37 @@
 # Embeddings package
+"""
+ai_engine/embeddings/__init__.py
+=================================
+Public exports for the embeddings package.
+
+Usage anywhere in the project
+------------------------------
+from ai_engine.embeddings import get_encoder, search_similar_items, build_index
+"""
+
+from ai_engine.embeddings.encoder import (
+    EMBEDDING_DIM,
+    FashionCLIPEncoder,
+    get_encoder,
+)
+from ai_engine.embeddings.vector_store import (
+    FAISSVectorStore,
+    search_similar_items,
+)
+from ai_engine.embeddings.database_builder import (
+    FAISSIndexBuilder,
+    build_index,
+)
+
+__all__ = [
+    # encoder
+    "EMBEDDING_DIM",
+    "FashionCLIPEncoder",
+    "get_encoder",
+    # search
+    "FAISSVectorStore",
+    "search_similar_items",
+    # index building
+    "FAISSIndexBuilder",
+    "build_index",
+]
